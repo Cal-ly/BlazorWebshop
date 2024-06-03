10c6de70ca32effd1c57c08db11b484f2756e780
Cal-ly <cal-ly@outlook.dk>
2024-06-03 19:50:19 +0200

    Add project files.

---
+++ BlazorWebshop/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorWebshop.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
